fd45ce636f0981258930ce78a4561166f0336686
Obiasnara <74296002+Obiasnara@users.noreply.github.com>
2021-12-03 00:29:51 +0100

    Push divs + some js to detect clics

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="main.js">
+    <script src="main.js"></script>
     <title>Defi CAPCOD</title>
 </head>
 
@@ -18,7 +18,11 @@
     echo "<div class='containerDivs'>";
     for ($i = 1; $i <= 30; $i++) {
         for ($j = 1; $j <= 28; $j++) {
-            echo "<div class='span'></div>";
+            if($j%26 == 0){
+                echo "<div class='spanm'></div>";
+            }
+            else
+                echo "<div class='span'></div>";
         }
     }
     echo "</div>";
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
-window.addEventListener("load", function(event){
-
+window.addEventListener("load", function(event) {
+    const spans = this.document.querySelectorAll('.spanm');
+    spans.forEach(element => {
+        element.addEventListener('click', () => {
+            console.log("TTT");
+        })
+    });
 })
