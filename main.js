9318de6831febd21debbcf402cc5bf45b180647f
Obiasnara <74296002+Obiasnara@users.noreply.github.com>
2021-12-03 03:59:31 +0100

    Important changes

--- main.js
+++ main.js
@@ -26,9 +26,12 @@ window.addEventListener("load", function(event) {
             .then(res => res.text())
             .then((result) => {
                 let chaine1 = result.toString();
-                let chaine2 = text.value.toLowerCase().split(" ").join("");;
+                let chaine2 = text.value.toLowerCase().split(" ").join("").split("−").join("-");
                 if (chaine2 == chaine1) {
                     window.open(`https://www.google.com/maps/@34.1325361,-118.3224481,3a,75y,31.69h,77.39t/data=!3m8!1e1!3m6!1sAF1QipPH63Kkr0XDAM9E1onLoJHSArwG-L97_RB9FC7u!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipPH63Kkr0XDAM9E1onLoJHSArwG-L97_RB9FC7u%3Dw203-h100-k-no-pi-0-ya90.36409-ro-0-fo100!7i8192!8i4096`);
+                } else {
+                    console.log(chaine2);
+                    console.log(chaine1);
                 }
             });
     })
