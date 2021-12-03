b5abcdf8f549de053b04916bd2729ce63ee2f1b7
Arcreuss <63006702+Arcreuss@users.noreply.github.com>
2021-12-03 03:01:12 +0100

    upd

--- formLogin.php
+++ formLogin.php
@@ -1,4 +1,7 @@
-<?php session_start();?>
+<?php
+session_start();
+$_SESSION["user"] = "";?>
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,18 +14,22 @@
 <body>
     <a id="goBackHome" href=".">Home</a>
     <div id="preElFormulaire">
-        <img id="elFormulaireLogo" src="./Images/banner_site_nuit_info.png" width="175px" height="auto">
-        <form id="elFormulaire" action="traitement.php" method="post">
+        <!-- <img id="elFormulaireLogo" src="./Images/banner_site_nuit_info.png" width="175px" height="auto"> -->
+        <form id="elFormulaire" action="#" method="post">
             <label for="fname">Username:</label>
             <input type="text" id="fname" name="fname"><br><br>
             <label for="lname">Password:</label>
             <input type="text" id="lname" name="lname"><br>
             <input type="submit" value="Submit" id="elFormulaireSubmit">
         </form>
+
+        <?php
+            // print_r($_POST);
+            if(isset($_POST["fname"]) && $_POST["fname"] == "Hollywood"){
+                ?><a id="downloadlink" href="NuitDeL'infoEasterEg(GoogleCollab).ipynb" download="Enigme.ipynb">Next Step</a><?php
+            }
+        ?>
     </div>
-    <?php
-    if($_SESSION["fname"] == Hollywood){
-        echo 'hop';
-    }
-    ?>
+
+    
 </body>
