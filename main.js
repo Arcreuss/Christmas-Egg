bb085d3c34cf76e8fe8106b3296d4864c1478c12
Obiasnara <74296002+Obiasnara@users.noreply.github.com>
2021-12-03 03:02:12 +0100

    T

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 window.addEventListener("load", function(event) {
     const spans = this.document.querySelectorAll('.spanm');
+    const img = this.document.createElement('img');
+    const text = this.document.createElement('input');
+    const btn = this.document.createElement('input');
+    text.type = 'text';
+    text.classList.add("inputText")
+    btn.type = 'submit';
+    btn.value = 'Vérifier';
+
     let compteur = 0;
     let ended = false;
-    let img = this.document.createElement('img');
     img.src = ""
     fetch("IMAGE.PHP")
         .then(res => res.text())
@@ -10,7 +17,13 @@ window.addEventListener("load", function(event) {
             let temp = img.src
             img.src = temp + result;
         });
+    document.body.appendChild(text);
+    document.body.appendChild(btn);
 
+
+    btn.addEventListener('click', () => {
+        console.log(text.value);
+    })
 
     spans.forEach(element => {
         element.addEventListener('click', () => {
@@ -28,4 +41,5 @@ window.addEventListener("load", function(event) {
             }
         })
     });
+
 })
